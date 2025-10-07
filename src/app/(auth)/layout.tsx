686f96af22eabb70f2492431f3e9223d8c6d2389
apekshit-iksula22 <apekshit.sonawane@iksula.com>
2025-10-07 22:37:01 +0530

    header sign-in and sign up button updates added

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,16 +1,27 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
+  const pathname = usePathname();
+  const isSignin = pathname === "/sign-in";
+
   return (
     <main className="bg-neutral-100 min-h-screen">
       <div className="mx-auto max-w-screen-2xl p-4">
         <nav className="flex justify-between items-center">
           <Image src="/logo.svg" height="56" width="152" alt="logo" />
-          <Button variant="secondary">Sign Up</Button>
+          <Button asChild variant="secondary">
+            <Link href={isSignin ? "/sign-up" : "/sign-in"}>
+              {isSignin ? "Sign in" : "Login"}
+            </Link>
+          </Button>
         </nav>
         <div className="flex flex-col items-center justify-center p-4 md:pt-14">
           {children}
